d4db608a0d34b3d7ee7064a7fd43eb12a73d8672
linzhang <linzhang@thoughtworks.com>
2019-05-21 18:33:57 +0800

    add const

--- calcTest/calc.go
+++ calcTest/calc.go
@@ -9,31 +9,37 @@ import (
 	"github.com/fatih/color"
 )
 
-var initValue float64 = 10000
-var times int = 0
+var times int
+var initMoney float64 = 200000 //initial money
+const loseOdd = 40             //30%
+const lossRatio float64 = 3    //5%
+const interestMax int = 5      //0%~10%
+const tradeTimes = 100         //trade 100 times
 
-func do() {
+func do(money float64) float64 {
 	random := rand.New(rand.NewSource(time.Now().UnixNano()))
 	win := random.Intn(1000)
-	if win < 300 {
-		initValue *= 0.95
+	if win < loseOdd*10 {
+		money *= 1 - lossRatio/100
 	} else {
-		result := random.Intn(100)
+		result := random.Intn(interestMax * 10)
 		interest := float64(result) / 1000
-		initValue *= 1 + interest
+		money *= 1 + interest
 		times++
 	}
+	return money
 }
 
 func main() {
-	for i := 0; i <= 100; i++ {
-		tmp := initValue
-		do()
-		if tmp > initValue {
-			color.Green(strconv.FormatFloat(initValue, 'f', 6, 64))
+	money := initMoney
+	for i := 0; i <= tradeTimes; i++ {
+		tmp := money
+		money = do(money)
+		if tmp > money {
+			color.Green(strconv.FormatFloat(money, 'f', 6, 64))
 		} else {
-			color.Red(strconv.FormatFloat(initValue, 'f', 6, 64))
+			color.Red(strconv.FormatFloat(money, 'f', 6, 64))
 		}
 	}
-	fmt.Println(times)
+	fmt.Printf("win %.2f%% and money is %.2f%%", float64(times)/tradeTimes*100, money/initMoney*100)
 }
