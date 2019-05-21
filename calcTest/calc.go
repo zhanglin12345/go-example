a15407d3b92d91d1a493fbe88c9e1cf1c784ac3e
linzhang <linzhang@thoughtworks.com>
2019-05-21 19:03:15 +0800

    add more comments

--- calcTest/calc.go
+++ calcTest/calc.go
@@ -10,36 +10,40 @@ import (
 )
 
 var times int
-var initMoney float64 = 200000 //initial money
-const loseOdd = 40             //30%
-const lossRatio float64 = 3    //5%
-const interestMax int = 5      //0%~10%
-const tradeTimes = 100         //trade 100 times
+var initMoney float64 = 10000 //initial money
+const loseOdd = 40            //30%
+const lossRatio float64 = 3   //5%
+const interestMax int = 5     //0%~10%
+const tradeTimes = 1000       //trade 100 times
 
-func do(money float64) float64 {
+func do(money float64) (float64, float64) {
 	random := rand.New(rand.NewSource(time.Now().UnixNano()))
 	win := random.Intn(1000)
+	var odd float64
 	if win < loseOdd*10 {
-		money *= 1 - lossRatio/100
+		odd = lossRatio / 100
+		money *= 1 - odd
 	} else {
 		result := random.Intn(interestMax * 10)
-		interest := float64(result) / 1000
-		money *= 1 + interest
+		odd = float64(result) / 1000
+		money *= 1 + odd
 		times++
 	}
-	return money
+	return money, odd
 }
 
 func main() {
 	money := initMoney
+	var odd float64
 	for i := 0; i <= tradeTimes; i++ {
 		tmp := money
-		money = do(money)
+		money, odd = do(money)
 		if tmp > money {
-			color.Green(strconv.FormatFloat(money, 'f', 6, 64))
+			color.Green("%v		-%.2f%%", strconv.FormatFloat(money, 'f', 6, 64), odd*100)
 		} else {
-			color.Red(strconv.FormatFloat(money, 'f', 6, 64))
+			color.Red("%v		%.2f%%", strconv.FormatFloat(money, 'f', 6, 64), odd*100)
 		}
 	}
-	fmt.Printf("win %.2f%% and money is %.2f%%", float64(times)/tradeTimes*100, money/initMoney*100)
+	fmt.Printf("初始资金:%v, 交易了%v次，其中%v次赔了%v%%，%v次赚了0到%v%% \n", initMoney, tradeTimes, tradeTimes-times, lossRatio, times, interestMax)
+	fmt.Printf("最终资金%v, 赚了%.2f%%，", money, money/initMoney*100)
 }
